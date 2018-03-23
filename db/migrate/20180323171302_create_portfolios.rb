e81ad86aa409df7d31b4f658157ca445334e5e8a
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-03-23 17:21:13 +0000

    Created a Portfolio resource via resource generator

---
+++ db/migrate/20180323171302_create_portfolios.rb
@@ -0,0 +1,11 @@
+class CreatePortfolios < ActiveRecord::Migration[5.1]
+  def change
+    create_table :portfolios do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
