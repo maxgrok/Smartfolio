a9c402318265047aa655493d11d43615753302b6
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-03-26 08:39:14 +0000

    Completed crud functionality for portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,8 +26,8 @@ puts "5 skills were created"
      body: "The
 company extended the metaphor to the website with an image
 of a suit of armor and the same tagline. The metaphor was consistent throughout the company’s marketing material",
-     main_image: "image_path('930.jpg')",
-     thumb_image: "image_path('930.jpg')"
+     main_image: "https://placehold.it/600x400",
+     thumb_image: "https://placehold.it/350x200"
   	)
 end
 
