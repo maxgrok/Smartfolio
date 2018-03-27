79b246a3505c7679bea49d7257c3d23e1f87e3a0
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-03-27 09:55:35 +0000

    Set default values for porfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,11 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :ruby_on_rails, -> {where(subtitle: "Ruby on Rails")}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+       self.main_image ||= "https://placehold.it/600x400"
+       self.thumb_image ||= "https://placehold.it/350x200"
+	end
 end
