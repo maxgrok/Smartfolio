01563843283f0d11e9deffd61200f3b4614e78f7
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-03-30 15:50:32 +0000

    Refactored application controller to include concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :configure_permited_parameters, if: :devise_controller?
-  before_action :set_source
+  include SetSource
 
   protected
   
   def configure_permited_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
     devise_parameter_sanitizer.permit(:account_update, keys:[:name])
-  end
-
-  def set_source
-    session[:source] = params[:q] if params[:q]
   end
 end
