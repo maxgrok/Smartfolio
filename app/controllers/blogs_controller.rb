83a36bcbdce2b9a42297b6eb73874d010e1dc702
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-04-10 17:26:44 +0000

    Implemented partials, font-awesome and pagination for blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My portfolio blog"
     @seo_keywords = "My blog body"
   end
